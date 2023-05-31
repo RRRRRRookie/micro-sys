469299481bc25b11b7e184af61c44dd9debb8854
RRRRRRookie <970584523@qq.com>
2023-05-31 14:32:54 +0800

    🎉 initial project

---
+++ micro-sys-promethus/src/main/java/com/broken/line/MicroSysBuApplication.java
@@ -0,0 +1,13 @@
+package com.broken.line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSysBuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroSysBuApplication.class, args);
+    }
+
+}
